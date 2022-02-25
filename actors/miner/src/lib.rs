f29ee9e45ac5f4802ccbd2afb3aa23dcf0e12d4a
David Himmelstrup <lemmih@gmail.com>
2022-02-25 17:43:05 +0100

    Use env logger.

--- actors/miner/src/lib.rs
+++ actors/miner/src/lib.rs
@@ -1325,9 +1325,9 @@ impl Actor {
 		    	// The sector's power will be recalculated when it's proven.
                 let duration = precommit.expiration - curr_epoch;
                 let sector_weight = qa_power_for_weight(info.sector_size, duration, &deal_weight.deal_weight, &deal_weight.verified_deal_weight);
-                eprintln!("fvm sector_weight: {}", sector_weight);
+                log::info!("fvm sector_weight: {}", sector_weight);
                 let deposit_req = pre_commit_deposit_for_power(&reward_stats.this_epoch_reward_smoothed,&power_total.quality_adj_power_smoothed , &sector_weight);
-                eprintln!("fvm deposit_req: {}", deposit_req);
+                log::info!("fvm deposit_req: {}", deposit_req);
                 // Build on-chain record.
                 chain_infos.push(SectorPreCommitOnChainInfo{
                     info: precommit.clone(),
